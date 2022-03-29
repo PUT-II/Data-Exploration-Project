67c87200c3790dbd732cc2014d9ae05fec1498e9
Gunock <asz.czyk@gmail.com>
2022-03-29 22:41:09 +0200

    Added HSV features

--- notebooks/workers.py
+++ notebooks/workers.py
@@ -1,7 +1,12 @@
-def download_and_save(url_: str):
-    import requests
-    import os
+import os
 
+import cv2 as cv
+import numpy as np
+import requests
+from PIL import Image
+
+
+def download_and_save(url_: str):
     file_path = f'./images/{url_.split("/")[-2]}.jpg'
     if os.path.exists(file_path):
         return
@@ -9,3 +14,29 @@ def download_and_save(url_: str):
     response = requests.get(url_)
     with open(file_path, mode='wb') as file:
         file.write(response.content)
+
+
+def extract_hsv_features(url: str):
+    file_path = f'./images/{url.split("/")[-2]}.jpg'
+
+    image = Image.open(file_path)
+    image_array = np.array(image)
+    image_cropped = __crop_image(image_array, 16)
+    if image_cropped.size > 0:
+        image_array = cv.cvtColor(image_cropped, cv.COLOR_RGB2HSV)
+    avg_hue = np.median(image_array[:, :, 0])
+    avg_saturation = np.median(image_array[:, :, 1])
+    avg_value = np.median(image_array[:, :, 2])
+
+    row = {
+        'thumbnail_med_hue': avg_hue,
+        'thumbnail_med_saturation': avg_saturation,
+        'thumbnail_med_value': avg_value
+    }
+
+    return url, row
+
+
+def __crop_image(img, tol=0):
+    mask = cv.cvtColor(img, cv.COLOR_RGB2GRAY) > tol
+    return img[np.ix_(mask.any(1), mask.any(0))]
