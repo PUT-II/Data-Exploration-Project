dc3a8eec27800c0176e4ef00f01f6437791c0205
Gunock <asz.czyk@gmail.com>
2022-03-30 13:48:12 +0200

    Implemented hasText feature

--- notebooks/workers.py
+++ notebooks/workers.py
@@ -6,35 +6,73 @@ import requests
 from PIL import Image
 
 
-def download_and_save(url_: str):
-    file_path = f'./images/{url_.split("/")[-2]}.jpg'
+def download_and_save(url: str):
+    file_path = __file_path_from_url(url)
     if os.path.exists(file_path):
         return
 
-    response = requests.get(url_)
+    response = requests.get(url)
     with open(file_path, mode='wb') as file:
         file.write(response.content)
 
 
 def extract_hsv_features(url: str):
-    file_path = f'./images/{url.split("/")[-2]}.jpg'
+    file_path = __file_path_from_url(url)
 
     image = Image.open(file_path)
     image_array = np.array(image)
     image_cropped = __crop_image(image_array, 16)
     if image_cropped.size > 0:
         image_array = cv.cvtColor(image_cropped, cv.COLOR_RGB2HSV)
-    avg_hue = np.median(image_array[:, :, 0])
-    avg_saturation = np.median(image_array[:, :, 1])
-    avg_value = np.median(image_array[:, :, 2])
+
+    h, s, v = cv.split(image_array)
+    median_hue = np.median(h)
+    median_saturation = np.median(s)
+    median_value = np.median(v)
 
     row = {
-        'thumbnail_med_hue': avg_hue,
-        'thumbnail_med_saturation': avg_saturation,
-        'thumbnail_med_value': avg_value
+        'thumbnail_med_hue': median_hue,
+        'thumbnail_med_saturation': median_saturation,
+        'thumbnail_med_value': median_value
     }
 
     return url, row
+
+
+def detect_text(url: str):
+    file_path = __file_path_from_url(url)
+    image = Image.open(file_path)
+    image_array = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
+
+    net = cv.dnn.readNet('./frozen_east_text_detection.pb')
+
+    median_b = np.median(image_array[:, :, 0])
+    median_g = np.median(image_array[:, :, 1])
+    median_r = np.median(image_array[:, :, 2])
+
+    new_W = image.width + 32 - image.width % 32
+    new_H = image.height + 32 - image.height % 32
+    image_array_input = np.zeros(shape=(new_H, new_W, 3), dtype=np.uint8)
+
+    image_array_input[0:image.height, 0:image.width] = image_array
+
+    blob = cv.dnn.blobFromImage(image_array_input,
+                                scalefactor=1.0,
+                                size=(new_W, new_H),
+                                mean=(median_r, median_g, median_b),
+                                swapRB=True,
+                                crop=False)
+    net.setInput(blob)
+    scores, _ = net.forward([
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"
+    ])
+
+    return url, np.max(scores[0, 0]) >= 0.8
+
+
+def __file_path_from_url(url: str):
+    return f'./images/{url.split("/")[-2]}.jpg'
 
 
 def __crop_image(img, tol=0):
