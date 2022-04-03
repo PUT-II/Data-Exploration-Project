1b953521fe9356123f08a8ece5f920a21e65af76
Gunock <asz.czyk@gmail.com>
2022-04-03 13:00:51 +0200

    Added average HSV features, added colorfulness feature

--- notebooks/workers.py
+++ notebooks/workers.py
@@ -16,24 +16,56 @@ def download_and_save(url: str):
         file.write(response.content)
 
 
-def extract_hsv_features(url: str):
+def __calculate_image_colorfulness(image):
+    """
+    https://pyimagesearch.com/2017/06/05/computing-image-colorfulness-with-opencv-and-python/
+    """
+    # split the image into its respective RGB components
+    (R, G, B) = cv.split(image.astype("float"))
+    # compute rg = R - G
+    rg = np.absolute(R - G)
+    # compute yb = 0.5 * (R + G) - B
+    yb = np.absolute(0.5 * (R + G) - B)
+    # compute the mean and standard deviation of both `rg` and `yb`
+    (rbMean, rbStd) = (np.mean(rg), np.std(rg))
+    (ybMean, ybStd) = (np.mean(yb), np.std(yb))
+    # combine the mean and standard deviations
+    stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+    meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+    # derive the "colorfulness" metric and return it
+    return stdRoot + (0.3 * meanRoot)
+
+
+def extract_color_features(url: str):
     file_path = __file_path_from_url(url)
 
     image = Image.open(file_path)
     image_array = np.array(image)
     image_cropped = __crop_image(image_array, 16)
+
     if image_cropped.size > 0:
-        image_array = cv.cvtColor(image_cropped, cv.COLOR_RGB2HSV)
+        image_array = image_cropped
+
+    colorfulness = __calculate_image_colorfulness(image_array)
+    image_array = cv.cvtColor(image_array, cv.COLOR_RGB2HSV)
 
     h, s, v = cv.split(image_array)
     median_hue = np.median(h)
     median_saturation = np.median(s)
     median_value = np.median(v)
+    h_r = h * (np.pi / 90)
+    average_hue = ((90 / np.pi) * np.arctan2(np.mean(np.sin(h_r)), np.mean(np.cos(h_r)))) % 180
+    average_saturation = np.mean(s)
+    average_value = np.mean(v)
 
     row = {
         'thumbnail_med_hue': median_hue,
         'thumbnail_med_saturation': median_saturation,
-        'thumbnail_med_value': median_value
+        'thumbnail_med_value': median_value,
+        'thumbnail_avg_hue': average_hue,
+        'thumbnail_avg_saturation': average_saturation,
+        'thumbnail_avg_value': average_value,
+        'thumbnail_colorfulness': colorfulness
     }
 
     return url, row
